62f70ed65a08c806abd2f6651107f5546459dca4
Rushik Patel <rushik.daraniya@shayonam.in>
2022-07-13 14:21:13 +0530

    first revision commit

---
+++ Desktop/material-dashboard-react-main/src/layouts/profile/index.js
@@ -0,0 +1,91 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+
+// @mui icons
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+
+// Material Dashboard 2 React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
+
+// Overview page components
+import Header from "layouts/profile/components/Header";
+
+// Data
+
+// Images
+
+function Overview() {
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox mb={2} />
+      <Header>
+        <MDBox mt={5} mb={3}>
+          <Grid container spacing={1}>
+            <Grid item xs={12} md={6} xl={4} />
+            <Grid item xs={12} md={6} xl={4} sx={{ display: "flex" }}>
+              <Divider orientation="vertical" sx={{ ml: -2, mr: 1 }} />
+              <ProfileInfoCard
+                title="profile information"
+                description="Hi, I’m Alec Thompson, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality)."
+                info={{
+                  fullName: "Alec M. Thompson",
+                  mobile: "(44) 123 1234 123",
+                  email: "alecthompson@mail.com",
+                  location: "USA",
+                }}
+                social={[
+                  {
+                    link: "",
+                    icon: <FacebookIcon />,
+                    color: "facebook",
+                  },
+                  {
+                    link: "",
+                    icon: <TwitterIcon />,
+                    color: "twitter",
+                  },
+                  {
+                    link: "",
+                    icon: <InstagramIcon />,
+                    color: "instagram",
+                  },
+                ]}
+                action={{ route: "", tooltip: "Edit Profile" }}
+                shadow={false}
+              />
+              <Divider orientation="vertical" sx={{ mx: 0 }} />
+            </Grid>
+          </Grid>
+        </MDBox>
+      </Header>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Overview;
